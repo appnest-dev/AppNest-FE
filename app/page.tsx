1d6ab6ea4d2dd51b256919b7ccd09c6b07b2106c
Mazen Adel <ma134652134652@gmail.com>
2024-01-09 01:10:19 +0200

    add: landing page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import "./page.css";
+import HLogo from "@/components/HLogo";
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="d-flex flex-column justify-content-center align-items-center gap-4 min-h-screen">
+      <HLogo width={300} />
+      <div className="d-flex gap-3">
+        <Link
+          href={"/auth/register"}
+          className="text-decoration-none d-inline-block py-2 px-3 fs-4 fw-semibold badge text-black m-2 shadow"
+        >
+          Register
+        </Link>
+        <Link
+          href={"/auth"}
+          className="text-decoration-none d-inline-block py-2 px-3 fs-4 fw-semibold badge text-black m-2 shadow"
+        >
+          Login
+        </Link>
+      </div>
+    </main>
+  );
 }
