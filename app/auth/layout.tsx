d022c8f7087ea4986d4dd8977884eb7954b66df3
Mazen Adel <ma134652134652@gmail.com>
2024-01-09 01:15:33 +0200

    add: logo

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,27 +1,22 @@
 import type { Metadata } from "next";
-import { Image, Stack } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Stack } from "react-bootstrap";
+import HLogo from "@/components/HLogo";
 
 export const metadata: Metadata = {
-  title: "Sketcher | Auth",
+  title: "AppNest | Auth",
   description: "Login or register.",
 };
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Stack
-        gap={3}
-        className="py-4 h-screen d-flex flex-column justify-content-center"
-      >
-        <div className="text-center pb-4">
-          <div className="pb-2">
-            <Image src={""} alt="Logo" />
-          </div>
-          <h1 className="fs-3">Backend Sketcher</h1>
-        </div>
-        {children}
-      </Stack>
-    </>
+    <Stack
+      gap={3}
+      className="py-4 min-h-screen d-flex flex-column justify-content-center"
+    >
+      <div className="w-100 d-flex justify-content-center pb-3">
+        <HLogo />
+      </div>
+      {children}
+    </Stack>
   );
 }
