a6eed95b3f4b6aef8735e86572788e77eadda01f
Mazen Adel <ma134652134652@gmail.com>
2024-01-09 01:05:18 +0200

    refactor: build form rendering code

--- components/auth/Form.tsx
+++ components/auth/Form.tsx
@@ -3,6 +3,8 @@ import { Formik, Form } from "formik";
 import "./styles.css";
 import ActionLink from "./ActionLink";
 import React, { ReactNode } from "react";
+import Input from "./Input";
+import Submit from "./Submit";
 
 type Values = {
   email?: string;
@@ -13,44 +15,65 @@ type Values = {
   OTC?: string;
 };
 
+type InputProps = {
+  type: string;
+  title: string;
+  id: string;
+  placeholder: string;
+};
+
+type Props = {
+  title: string;
+  actions: object;
+  google?: boolean;
+  inputs: InputProps[];
+  submitTitle: string;
+};
+
 export default function FormComponent({
   title,
   actions,
   google,
-  children,
-}: {
-  title: string;
-  actions: object;
-  google?: boolean;
-  children: ReactNode;
-}) {
+  inputs,
+  submitTitle,
+}: Props) {
   const initialValues = {};
   const handleSubmit = (values: Values) => {
     console.log(values);
   };
 
   return (
-    <>
-      <section className="form mx-auto">
-        <h2 className="text-center fs-6 fw-semibold pb-4">{title}</h2>
+    <section className="form mx-auto">
+      <h2 className="text-center fs-5 fw-semibold pb-4">{title}</h2>
 
-        {google && (
-          <button className="mt-2 bg-gray w-100 py-2 fw-semibold">
-            Continue with Google
-          </button>
+      {google && (
+        <button className="mt-2 bg-gray w-100 py-2 fw-semibold">
+          Continue with Google
+        </button>
+      )}
+
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            {inputs.map((el) => (
+              <Input
+                key={el.id}
+                id={el.id}
+                type={el.type}
+                title={el.title}
+                placeholder={el.placeholder}
+              />
+            ))}
+            <Submit title={submitTitle} />
+          </Form>
         )}
+      </Formik>
 
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          {({ handleSubmit }) => (
-            <Form onSubmit={handleSubmit}>{children}</Form>
-          )}
-        </Formik>
-        <div className="d-flex justify-content-between pt-5">
-          {Object.entries(actions).map((key) => (
-            <ActionLink key={key[0]} to={key[1]} title={key[0]} />
-          ))}
-        </div>
-      </section>
-    </>
+      <div className="d-flex justify-content-between pt-5">
+        {Object.entries(actions).map((key) => (
+          <ActionLink key={key[0]} to={key[1]} title={key[0]} />
+        ))}
+      </div>
+    </section>
   );
 }
