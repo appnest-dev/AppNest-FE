d861709e3175c77808a823b5ce8b551996255707
Mazen Adel <ma134652134652@gmail.com>
2024-01-14 20:50:24 +0200

    add: gradient gray background

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -11,7 +11,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <Stack
       gap={3}
-      className="py-4 min-h-screen d-flex flex-column justify-content-center"
+      className="py-4 min-h-screen d-flex flex-column justify-content-center bg-gradient-gray"
     >
       <div className="w-100 d-flex justify-content-center pb-3">
         <HLogo />
