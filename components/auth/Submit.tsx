0ce69ef9ab8825ad2b4cca6f6b083ab3d0d69c7b
Mazen Adel <ma134652134652@gmail.com>
2024-01-28 00:12:26 +0200

    change: 'auth' directory to 'form' directory

--- components/auth/Submit.tsx
+++
@@ -1,7 +0,0 @@
-export default function Submit({ title }: { title: string }) {
-  return (
-    <button type="submit" className="mt-4 bg-gray w-100 py-2 fw-semibold">
-      {title}
-    </button>
-  );
-}
