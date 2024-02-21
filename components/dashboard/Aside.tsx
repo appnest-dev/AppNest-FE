e9c5cfd6f953c24f85cf64df7a61bec6a9e0cf24
makkahwi <suhaibahmadai@hotmail.com>
2024-02-21 12:54:34 +0300

    Minor UI Fixes

--- components/dashboard/Aside.tsx
+++ components/dashboard/Aside.tsx
@@ -15,8 +15,11 @@ export default function Aside() {
   }, []);
 
   return (
-    <aside className="d-flex flex-column h-screen bg-light p-4 gap-4">
-      <Image src={hLogo} alt="App Nest" />
+    <aside
+      className="d-flex flex-column h-screen bg-light p-4 gap-4"
+      style={{ overflowX: "hidden" }}
+    >
+      <Image src={hLogo} alt="App Nest" className="logo" />
       <h6 className="text-danger">Trial Launch</h6>
 
       <Logout />
@@ -63,7 +66,7 @@ export default function Aside() {
         ]}
       />
 
-      <small className="position-absolute bottom-0 start-0 m-3">
+      <small className="position-absolute bottom-0 start-0 m-3 bg-light">
         By{" "}
         <a href="https://www.alembicsoft.com/" target="_blank">
           AlembicSoft
