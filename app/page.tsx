285f441bcfae26094991d74c44c7366d91a91dcf
makkahwi <suhaibahmadai@hotmail.com>
2024-02-21 11:29:26 +0300

    Add Landing Contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Link from "next/link";
-import "./page.css";
 import HLogo from "@/components/HLogo";
+import Link from "next/link";
+
+import "./page.css";
 
 export default function Home() {
   return (
     <main className="d-flex flex-column bg-gradient-gray justify-content-center align-items-center gap-4 min-h-screen">
       <HLogo width={300} />
+
+      <h3>Welcome to AppNest</h3>
+
+      <p>
+        The place to nest and nurture your web application plans before
+        executing
+      </p>
+
       <div className="d-flex gap-3">
         <Link
           href={"/auth/register"}
