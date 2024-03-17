4a4639f5a5cb2f078cac91d73389b802acccfffa
Mazen Adel <ma134652134652@gmail.com>
2024-03-17 17:56:33 +0200

    paginator initial commit

---
+++ components/dashboard/table/Paginator.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useRef, useState } from "react";
+import { FormSelect, Pagination } from "react-bootstrap";
+
+export default function Paginator() {
+  const currentPageRef = useRef<HTMLLIElement>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const fullPagesCount = useRef<HTMLLIElement>(null);
+  const [fullPages, setFullPages] = useState(5);
+
+  const prevPage = () => {
+    currentPage !== 1 && setCurrentPage((prev) => prev - 1);
+  };
+  const nextPage = () => {
+    currentPage < 5 && setCurrentPage((prev) => prev + 1);
+  };
+  const firstPage = () => {
+    setCurrentPage(1);
+  };
+  const lastPage = () => {
+    if (fullPages) {
+      setCurrentPage(fullPages);
+    }
+  };
+
+  const changeQuantity = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target?.value);
+  };
+
+  return (
+    <div className="d-flex">
+      <Pagination className="d-flex w-100 flex-fill justify-content-center">
+        <Pagination.First onClick={firstPage} />
+        <Pagination.Prev onClick={prevPage} />
+        <div className="d-flex align-items-center px-2 rounded-2">
+          <Pagination.Item ref={currentPageRef}>{currentPage}</Pagination.Item>
+          <Pagination.Item disabled ref={fullPagesCount}>
+            {fullPages}
+          </Pagination.Item>
+        </div>
+        <Pagination.Next onClick={nextPage} />
+        <Pagination.Last onClick={lastPage} />
+      </Pagination>
+      <FormSelect onChange={changeQuantity} className="w-auto">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+      </FormSelect>
+    </div>
+  );
+}
