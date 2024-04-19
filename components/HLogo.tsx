d4ab053878e640c50c34215c541ca3311633a976
Mazen Adel <ma134652134652@gmail.com>
2024-04-19 20:08:04 +0200

    refactor: make logo dynamic

--- components/HLogo.tsx
+++ components/HLogo.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import hLogo from "@/public/h-logo.png";
 
-export default function HLogo({ width = 200 }: { width?: number }) {
+export default function HLogo({ className }: { className?: string }) {
   return (
-    <div style={{ width: `${width}px`, mixBlendMode: "multiply" }}>
+    <div style={{ mixBlendMode: "multiply" }} className={`h-logo ${className}`}>
       <Image src={hLogo} alt="App Nest" className="w-100 h-100" />
     </div>
   );
