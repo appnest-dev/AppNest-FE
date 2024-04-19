2e456fe9a73e758059186bfe0c4230ac00e45bfe
Mazen Adel <ma134652134652@gmail.com>
2024-04-19 20:08:40 +0200

    adding indicator

--- components/form/Submit.tsx
+++ components/form/Submit.tsx
@@ -1,13 +1,26 @@
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 
-export default function Submit({ title }: { title: string }) {
+export default function Submit({
+  title,
+  disabled,
+}: {
+  title: string;
+  disabled?: boolean;
+}) {
   return (
     <Button
       variant="light"
       type="submit"
       className="mt-4 w-100 p-2 fw-semibold"
+      disabled={disabled}
     >
-      {title}
+      {disabled ? (
+        <Spinner animation="border" role="status" variant="secondary">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        title
+      )}
     </Button>
   );
 }
