1b2e6b5000bbbf0ceecab2b7947d4fb5b1af085c
Mazen Adel <ma134652134652@gmail.com>
2024-04-19 20:07:35 +0200

    refactor: make function in ES6

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,4 +1,4 @@
-export function toCamelCase(string: string) {
+export const toCamelCase = (string: string) => {
   const STRING = string.trim();
 
   return STRING.split(" ").length == 1
@@ -7,10 +7,12 @@ export function toCamelCase(string: string) {
         .map((str) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`)
         .join("")
         .slice(1)}`;
-}
+};
 
-export function removeMark(string: string) {
-  return string.trim()[string.length - 1] == "?"
+export const removeMark = (string: string) =>
+  string.trim()[string.length - 1] == "?"
     ? string.trim().slice(0, string.length - 1)
     : string.trim();
-}
+
+export const generatePlaceholder = (string: string) => (s: string) =>
+  `${string} ${s}`;
