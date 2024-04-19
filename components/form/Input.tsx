8a03bd77907102b523f0518480175332cb17ed09
Mazen Adel <ma134652134652@gmail.com>
2024-04-19 20:09:29 +0200

    add switch inputs

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,4 +1,5 @@
 import { Field } from "formik";
+import { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export type InputProps = {
@@ -10,7 +11,7 @@ export type InputProps = {
   max?: number;
   required?: boolean;
   confirmationId?: string;
-  value?: string;
+  value?: string | boolean | number;
   disabled?: boolean;
 };
 
@@ -29,26 +30,40 @@ export default function Input({
   required,
   confirmationId,
   handleChange,
+  value,
   values,
   disabled = false,
 }: InputProps & ControlledInputProps) {
+  const [state, setState] = useState(value as boolean | true);
   return (
     <Form.Group className="d-flex flex-column">
       <Form.Label htmlFor={id} className="fw-semibold">
         {title}
         {required && <span className="required-star"> * </span>}
       </Form.Label>
-      <Form.Control
-        as={Field}
-        type={type}
-        id={id}
-        onChange={handleChange}
-        value={values[id]}
-        placeholder={placeholder}
-        name={id}
-        className="bg-light px-3 py-2"
-        disabled={disabled}
-      />
+      {type == "switch" ? (
+        <Form.Check
+          type="switch"
+          id={id}
+          value={state ? "on" : "off"}
+          onChange={(e) => {
+            setState((prev) => !prev);
+            handleChange(e);
+          }}
+        />
+      ) : (
+        <Form.Control
+          as={Field}
+          type={type}
+          id={id}
+          onChange={handleChange}
+          value={values[id]}
+          placeholder={placeholder}
+          name={id}
+          className="bg-light px-3 py-2"
+          disabled={disabled}
+        />
+      )}
     </Form.Group>
   );
 }
