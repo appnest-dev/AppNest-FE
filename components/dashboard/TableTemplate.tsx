7cfc0de06f77ea4ba9dfd21b314ef30ee7c6c02d
Mazen Adel <ma134652134652@gmail.com>
2024-05-03 19:29:03 +0300

    refactor: change TableTemplate Props

--- components/dashboard/TableTemplate.tsx
+++ components/dashboard/TableTemplate.tsx
@@ -4,31 +4,24 @@ import React from "react";
 import Modal, { ModalProps } from "./modal/Modal";
 import Table, { TableProps } from "./table/Table";
 
-type Props = TableProps & {
+type Props = {
   title: string;
+  tableProps: TableProps;
   modalProps?: ModalProps;
 };
 
 export default function TableTemplate({
   title,
   modalProps,
-  heads,
-  rows,
+  tableProps,
 }: Props) {
   return (
     <section className="d-flex flex-column w-100 gap-2 p-4">
       <div className="d-flex justify-content-between">
         <h2>{title}</h2>
-        {modalProps && (
-          <Modal
-            buttonTitle={modalProps.buttonTitle}
-            inputs={modalProps.inputs}
-            title={modalProps.title}
-            submit={modalProps.submit}
-          />
-        )}
+        {modalProps && <Modal {...modalProps} />}
       </div>
-      <Table heads={heads} rows={rows} />
+      <Table {...tableProps} />
     </section>
   );
 }
