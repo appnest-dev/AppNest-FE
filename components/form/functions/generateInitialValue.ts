05805a5f1653440423cb438495a4ed8ad1e48028
Mazen Adel <ma134652134652@gmail.com>
2024-05-03 19:28:12 +0300

    add: switch to initial value types

--- components/form/functions/generateInitialValue.ts
+++ components/form/functions/generateInitialValue.ts
@@ -2,6 +2,7 @@ export const generateInitialValue = (type: string) => {
   switch (type) {
     case "checkbox":
     case "boolean":
+    case "switch":
       return false;
     default:
       return "";
