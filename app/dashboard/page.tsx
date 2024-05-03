62ae51c7b13232a63fc485b7aec765a3fd4cd4fd
Mazen Adel <ma134652134652@gmail.com>
2024-05-03 19:22:49 +0300

    add: implement loading method

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function page() {
+"use client";
+
+import { useLoading } from "@/components/dashboard/LoadingProvider";
+import { useEffect } from "react";
+
+export default function Page() {
+  const { setLoading } = useLoading();
+  useEffect(() => {
+    const timeout1 = setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+    const timeout2 = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+    };
+  }, []);
   return <div className="p-5">page</div>;
 }
